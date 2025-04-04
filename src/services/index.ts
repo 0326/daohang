0d44b14309f7c22b6a3fcde6bdeb0a082cc262f6
0326 <winniringy@gmail.com>
2025-04-04 21:46:07 +0800

    chore: remove cloudsdk

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,17 +1,18 @@
-import cloudbase from "@cloudbase/js-sdk";
+// import cloudbase from "@cloudbase/js-sdk";
 
 export * from "./category";
 export * from "./tenant";
 export * from "./website"
 
 export async function initCloudbase(env?: string) {
-  const app = cloudbase.init({
-    env: env || "daohang-1gmk7uhx314adbde", // 替换为你的云开发环境 ID
-    clientId: env || "daohang-1gmk7uhx314adbde", // 替换为你的云开发环境 ID
-  });
-  const auth = app.auth({
-    persistence: "local",
-  });
-  await auth.signInAnonymously({}); // 或者使用其他登录方式
-  return app.models
+  return env
+  // const app = cloudbase.init({
+  //   env: env || "daohang-1gmk7uhx314adbde", // 替换为你的云开发环境 ID
+  //   clientId: env || "daohang-1gmk7uhx314adbde", // 替换为你的云开发环境 ID
+  // });
+  // const auth = app.auth({
+  //   persistence: "local",
+  // });
+  // await auth.signInAnonymously({}); // 或者使用其他登录方式
+  // return app.models
 }
